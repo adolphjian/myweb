6bd750f5feb539cd6605e46f30378eb8230683f5
code1031 <1031247267@qq.com>
2018-04-15 16:05:06 +0800

    完成右侧内容渲染

--- src/js/category.js
+++ src/js/category.js
@@ -12,16 +12,29 @@ $(function(){
       $('#leftMenuInfo').html(html);
       // 绑定菜单事件
       $('#leftMenuInfo').find('.items').on('click',function(){
+        // 点击控制菜单选中
+        $('#leftMenuInfo .cur').removeClass('cur');
+        $(this).addClass('cur');
         // 渲染右侧
-        renderRightCate(data.data);
+        // renderRightCate(data.data);
+        renderRightCate.call(this,data.data);
       })
+      // 添加菜单选中效果
+      $('#leftMenuInfo').find('.items').eq(0).addClass('cur');
       resolve(data.data);
     })
   }
   // 渲染右侧分类信息
   function renderRightCate(data){
+    // 如何从这里获取左侧菜单的索引
+    let currentIndex = $(this).index();
+    currentIndex = currentIndex === -1 ? 0 : currentIndex;
+    let currentData = data[currentIndex] && data[currentIndex].children;
     return new Promise(function(resolve,reject){
-      console.log(data)
+      // 渲染右侧内容
+      let html = template('rightCateTpl',currentData);
+      $('#rightCateInfo').html(html);
+      resolve();
     })
   }
 
@@ -39,6 +52,7 @@ $(function(){
         $.toast('服务器错误')
       })
       .finally(function(){
+        // 无论成功还是失败都会调用该方法
         $.hidePreloader('已经完成');
       })
   });
